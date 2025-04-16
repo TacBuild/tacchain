942ff98c63506560578a43092d880ba9a25d0802
Tugay Emin <tgntrr@gmail.com>
2025-04-16 13:39:00 +0300

    WIP add wasmd tests

---
+++ contrib/test-wasmd/04-gov.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+sleep 1
+echo "## Submit a CosmWasm gov proposal"
+RESP=$(tacchaind tx wasm submit-proposal store-instantiate "$DIR/../../tests/testdata/reflect_2_0.wasm" \
+  '{}' --label="testing" \
+  --title "testing" --summary "Testing" --deposit "1000000000utac" \
+  --admin $(tacchaind keys show -a validator --keyring-backend=test) \
+  --amount 123utac \
+  --keyring-backend=test \
+  --from validator --gas auto --gas-adjustment=1.5 -y   --node=http://localhost:26657 -b sync -o json)
+echo $RESP
+sleep 6
+tacchaind q tx $(echo "$RESP"| jq -r '.txhash') -o json | jq
+

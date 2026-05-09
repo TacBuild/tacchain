f9712138c833640c8910fac3bb677a0fce42f57f
Aleksei Kokinos <akkokinos@gmail.com>
2026-05-09 21:22:11 +0700

    fix: gen localnode script and docker files refactor

--- gen_localnode.sh
+++ gen_localnode.sh
@@ -2,6 +2,19 @@
 
 # This script is used to generate a initial state and node docker images for local testing.
 
+DOCKERFILE="Dockerfile.local"
+BUILD_CONTEXT="."
+
+for arg in "$@"; do
+  case $arg in
+    --workspace)
+      DOCKERFILE="Dockerfile.local.workspace"
+      BUILD_CONTEXT=".."
+      shift
+      ;;
+  esac
+done
+
 export TACCHAIND=/$PWD/build/tacchaind &&
 export INITIAL_BALANCE=10000000000000000000000000000000000000 &&
 export CHAIN_ID=tacchain_2391337-1 &&
@@ -14,22 +27,22 @@ export GOV_TIME_SECONDS=300 &&
 
 COPYFILE_DISABLE=1 tar --no-xattrs --format=ustar -C "$HOME" -czf .tacchaind.tar .tacchaind
 
-export WORKSPACE=../
-
 docker buildx build \
   --platform linux/amd64 \
   --load \
   -t tacchaind:amd64 \
-  -f $WORKSPACE/tacchain/Dockerfile.workspace \
-  $WORKSPACE
+  -f "$DOCKERFILE" \
+  "$BUILD_CONTEXT"
+
 docker save -o tacchaind-amd64.tar tacchaind:amd64
 
 docker buildx build \
   --platform linux/arm64 \
   --load \
   -t tacchaind:arm64 \
-  -f $WORKSPACE/tacchain/Dockerfile.workspace \
-  $WORKSPACE
+  -f "$DOCKERFILE" \
+  "$BUILD_CONTEXT"
+
 docker save -o tacchaind-arm64.tar tacchaind:arm64
 
 gzip tacchaind-*.tar
